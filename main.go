93d1dfd7f6290a6cc3943cb525c9202b816b49f0
Christoph Linse <christophlinse@googlemail.com>
2024-03-09 17:18:42 +0100

    WIP

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func addHandler(w http.ResponseWriter, r *http.Request) {
 	result := Result{Sum: sum}
 	jsonResult, err := json.Marshal(result)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Erdror(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
